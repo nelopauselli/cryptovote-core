7d4aa4bed2b46333ed1a39c98bda3f88f1071c2d
Nelo Pauselli <nelopauselli@gmail.com>
2020-01-17 10:36:15 -0300

    Ajustes en build y docker

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
 
 WORKDIR /app
 
-ADD ./crypto-vote-linux-x64.tar.gz /app
+ADD ./cryptovote-linux-x64.tar.gz /app
 
-RUN chmod +x /app/crypto-vote
+RUN chmod +x /app/CryptoVote
 
 EXPOSE 80
 
-ENTRYPOINT ["/app/crypto-vote"]
+ENTRYPOINT ["/app/CryptoVote"]
--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 mkdir -p ./publish
 
-#dotnet publish ./src/All.sln -r win-x64 -c release
-#rm -r ./publish/crypto-vote-win-x64
-#mv ./src/crypto-vote/bin/Release/netcoreapp3.1/win-x64/publish ./publish/crypto-vote-win-x64
-#zip -r -m -j ./publish/crypto-vote-win-x64.zip ./publish/crypto-vote-win-x64
-#rm -r ./publish/crypto-vote-win-x64
+# dotnet publish ./src/CryptoVote-Core.sln -r win-x64 -c release
+# rm -r ./publish/cryptovote-win-x64
+# mv ./src/CryptoVote/bin/Release/netcoreapp3.1/win-x64/publish ./publish/cryptovote-win-x64
+# zip -r -m -j ./publish/cryptovote-win-x64.zip ./publish/cryptovote-win-x64
+# rm -r ./publish/cryptovote-win-x64
 
-#dotnet publish ./src/All.sln -r linux-arm -c release
-#rm -r ./publish/crypto-vote-linux-arm
-#mv ./src/crypto-vote/bin/Release/netcoreapp3.1/linux-arm/publish ./publish/crypto-vote-linux-arm
-#tar -C ./publish/crypto-vote-linux-arm -cvf ./publish/crypto-vote-linux-arm.tar.gz .
-#rm -r ./publish/crypto-vote-linux-arm
+# dotnet publish ./src/CryptoVote-Core.sln -r linux-arm -c release
+# rm -r ./publish/cryptovote-linux-arm
+# mv ./src/CryptoVote/bin/Release/netcoreapp3.1/linux-arm/publish ./publish/cryptovote-linux-arm
+# tar -C ./publish/cryptovote-linux-arm -cvf ./publish/cryptovote-linux-arm.tar.gz .
+# rm -r ./publish/cryptovote-linux-arm
 
-dotnet publish ./src/All.sln -r linux-x64 -c release
-rm -r ./publish/crypto-vote-linux-x64
-mv ./src/crypto-vote/bin/Release/netcoreapp3.1/linux-x64/publish ./publish/crypto-vote-linux-x64
-tar -C ./publish/crypto-vote-linux-x64 -czvf ./publish/crypto-vote-linux-x64.tar.gz .
-rm -r ./publish/crypto-vote-linux-x64
+dotnet publish ./src/CryptoVote-Core.sln -r linux-x64 -c release
+rm -r ./publish/cryptovote-linux-x64
+mv ./src/CryptoVote/bin/Release/netcoreapp3.1/linux-x64/publish ./publish/cryptovote-linux-x64
+tar -C ./publish/cryptovote-linux-x64 -czvf ./publish/cryptovote-linux-x64.tar.gz .
+rm -r ./publish/cryptovote-linux-x64
 
 cp ./Dockerfile ./publish
 cp ./docker-compose.yml ./publish
